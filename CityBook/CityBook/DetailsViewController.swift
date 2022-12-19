0fa605900b4eedef85e881ffd0494787445dda2e
Evren Akgün <evrenakgun3@gmail.com>
2022-12-19 20:29:22 +0300

    Introduction to City Book App

---
+++ CityBook/CityBook/DetailsViewController.swift
@@ -0,0 +1,24 @@
+//
+//  DetailsViewController.swift
+//  CityBook
+//
+//  Created by Evren Akgün on 19.12.2022.
+//
+
+import UIKit
+
+class DetailsViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var cityRegionLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+
+}
---
+++ CityBook/CityBook/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  CityBook
+//
+//  Created by Evren Akgün on 19.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
