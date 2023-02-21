4df0cad8ff14d1fcb5211b968ff9114b28427d0c
Evren Akgün <evrenakgun3@gmail.com>
2023-02-21 21:48:46 +0300

    Firebase tutorial first lecture

--- PhotoSharingApp/PhotoSharingApp/ViewController.swift
+++ PhotoSharingApp/PhotoSharingApp/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func loginTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func signUpTapped(_ sender: Any) {
+        performSegue(withIdentifier: "toFeedVC", sender: nil)
+    }
 }
 
---
+++ PhotoSharingApp/Pods/Target Support Files/Pods-PhotoSharingApp/Pods-PhotoSharingApp-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PhotoSharingApp : NSObject
+@end
+@implementation PodsDummy_Pods_PhotoSharingApp
+@end
